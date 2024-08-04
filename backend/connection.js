ff6985e5b14262d0ea0847d58ca47ae576b76d8a
ShadKhan0 <kshad6523@gmail.com>
2024-08-04 16:46:34 +0530

    frontend and backend folders created

---
+++ backend/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const url= "mongodb+srv://kshad6523:Spiker123@cluster0.gbadbej.mongodb.net/mydb?retryWrites=true&w=majority&appName=Cluster0"
+
+//asynchronous function - Promise object
+mongoose.connect(url)
+.then((result) => {
+    console.log("Database connected");
+}).catch((err) => {
+    console.log(err);
+});
+
+module.exports =mongoose;
